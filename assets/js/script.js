58e6bc3f3f95f3206902bdb5899bb66a7045719f
EsotericZ <cjsand03@gmail.com>
2021-11-17 13:14:05 -0700

    finished all code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ $("#currentDay").html(date);
 
 let times = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
+// LOOP THRU ALL THE TIMES AND SET TO PAST/PRESENT/FUTURE (DEFUALT)
 function colorChanger() {
     const now = moment().format('H');
     for (let i = 0; i < times.length; i++) {
@@ -22,6 +23,9 @@ $(function() {
 // STORE ALL NOTES
 let store = [];
 $(".saveBtn").click(function() {
+    if (localStorage.getItem("store") !== null ) {
+        store = JSON.parse(localStorage.getItem('store'));
+    }
     let time = $(this).parent().children()[0].innerHTML;
     let message = $(this).parent().children()[1].value;
     let add = { time: time, message: message};
@@ -30,14 +34,16 @@ $(".saveBtn").click(function() {
 });
 
 // RETRIEVE ALL NOTES FROM STORAGE
-let storedData = JSON.parse(localStorage.getItem('store'));
-storedData.forEach(element => {
-    let checkTime = element.time;
-    let checkMsg = element.message;
-    for (let i = 0; i < times.length; i++) {
-        let checkId = $(`#${times[i]}`).parent().children()[0].innerHTML;
-        if (checkTime == checkId) {
-            $(`#${times[i]}`).parent().children()[1].textContent = checkMsg;
+if (localStorage.getItem("store") !== null ) {
+    let storedData = JSON.parse(localStorage.getItem('store'));
+    storedData.forEach(element => {
+        let checkTime = element.time;
+        let checkMsg = element.message;
+        for (let i = 0; i < times.length; i++) {
+            let checkId = $(`#${times[i]}`).parent().children()[0].innerHTML;
+            if (checkTime == checkId) {
+                $(`#${times[i]}`).parent().children()[1].textContent = checkMsg;
+            }
         }
-    }
-});
+    });
+}
