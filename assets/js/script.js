4f06923c721adade5126d2eac152bf95e4385685
EsotericZ <cjsand03@gmail.com>
2021-11-17 08:11:26 -0700

    completed all color changing elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
 const date = moment().format('dddd, MMM Do');
 $("#currentDay").html(date);
+
+let times = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+function colorChanger() {
+    const now = moment().format('h');
+    for (let i = 0; i < times.length; i++) {
+        if (now > times[i]) {
+            $(`#${times[i]}`).attr('class', 'col-10 past');
+        } else if (now == times[i]) {
+            $(`#${times[i]}`).attr('class', 'col-10 present');
+        }        
+    }
+}
+
+$(function() {
+    setInterval(colorChanger, 1000);
+});
